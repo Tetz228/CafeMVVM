55663213f960e51d6c97425ed9f11c4fd548b482
Tetz <tetz2281337@gmail.com>
2021-06-19 11:31:06 +0700

    Добавьте файлы проекта.

---
+++ CafeMVVM/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace CafeMVVM
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
